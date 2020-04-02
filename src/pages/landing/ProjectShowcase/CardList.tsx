5861ce5b9cb9b73c8a1f6ad8c4f51d4b6b6187da
Suraneti <suraneti.rod@gmail.com>
2020-04-02 21:12:18 +0700

    Add constants data

--- src/pages/landing/ProjectShowcase/CardList.tsx
+++ src/pages/landing/ProjectShowcase/CardList.tsx
@@ -3,27 +3,12 @@ import React from 'react'
 // Elements
 import Card from './Card'
 
-// Termporary hardcode 
-const DATA = [
-  {
-    source: 'image icon url',
-    title: 'Dataset',
-    description: 'Dataset Database และ API ที่พร้อมใช้งานสำหรับสถานการณ์ COVID-19 ของประเทศ',
-    language: 'Python',
-    uri: 'https://github.com/Covid19FlashFire/datasets'
-  },
-  {
-    source: 'image icon url',
-    title: 'covidfirelab.com',
-    description: 'แหล่งรวบรวม API และ Project เพื่อร่วมกันแก้ไขปัญหา COVID-19',
-    language: 'Javascript',
-    uri: 'https://github.com/Covid19FlashFire/covidfirelab.com'
-  },
-]
+// Constatns
+import CONSTANTS from '../../../constants'
 
 const CardList = () => (
   <>
-    {DATA.map((repo, index) => (
+    {CONSTANTS.projects.DATA.map((repo, index) => (
       <div className="column is-half" style={styles.centered}>
         <Card repo={repo} key={index} />
       </div>
--- src/pages/landing/ProjectShowcase/index.tsx
+++ src/pages/landing/ProjectShowcase/index.tsx
@@ -6,20 +6,17 @@ import CardList from './CardList'
 const ProjectShowcase = () => (
   <section className="section">
     <div className="container">
-      <h1 className="title has-text-centered is-variable">Project Showcase</h1>
-      <div className="columns is-multiline is-centered">
+      <h1 className="title">Project Showcase</h1>
+      <div className="columns is-multiline">
         <CardList />
       </div>
-      <div className="columns is-centered" style={{marginTop: 30}}>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/Covid19FlashFire"
-          className="button is-link"
-          style={{marginLeft: 10}}>
-          See more
-        </a>
-      </div>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/Covid19FlashFire"
+        className="button is-link">
+        See more
+      </a>
     </div>
   </section>
 )
